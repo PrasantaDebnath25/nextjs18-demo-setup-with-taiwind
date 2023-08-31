b00e28bb15bc2c001fccf1c65ec85d3cfb7b8571
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-09-01 01:31:19 +0530

    handleClick added

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const router = useRouter()
+  
   const handleClick = (e, name) => {
     e.preventDefault()
     router.push("/"+name)
