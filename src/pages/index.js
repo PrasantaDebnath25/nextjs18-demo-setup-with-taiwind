acbc70e71f43ec5c4112118ba99dfd3663c3266a
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2023-09-07 01:41:22 +0530

    Package installed

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
             <p className="text-lg font-medium">
               “Tailwind CSS is the only framework that I've seen scale
               on large teams. It’s easy to customize, adapts to any design,
-              and the build size is tiny.”
+              and the build size is tiny.” 
             </p>
           </blockquote>
           <figcaption className="font-medium">
